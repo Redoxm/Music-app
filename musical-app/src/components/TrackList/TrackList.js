d793dcdb52968a5496df1d0d201fb7da40c421bd
Yaya <sikiruyaya1@gmail.com>
2023-12-20 20:18:13 -0800

    Track component

--- musical-app/src/components/TrackList/TrackList.js
+++ musical-app/src/components/TrackList/TrackList.js
@@ -26,29 +26,6 @@ class TrackList extends React.Component {
     }
 }
 
-// export default TrackList;
-
-// import React from "react";
-// import "./TrackList.css";
-// import Track from "../Track/Track";
-
-// class TrackList extends React.Component {
-//     render() {
-//         return (
-//             <div className="TrackList">
-//                 {this.props.tracks.map(track => (
-//                     <Track
-//                         track={track}
-//                         key={track.id}
-//                         onAdd={this.props.onAdd}
-//                         isRemoval={this.props.isRemoval}
-//                         onRemove={this.props.onRemove}
-//                     />
-//                 ))}
-//             </div>
-//         );
-//     }
-// }
 
 TrackList.defaultProps = {
     tracks: [] // Set a default empty array if tracks is not provided
