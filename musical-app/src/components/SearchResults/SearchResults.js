375e415e73f7438db62d7006acb865fceb4f6411
Yaya <sikiruyaya1@gmail.com>
2023-12-20 20:16:12 -0800

    Search result component

--- musical-app/src/components/SearchResults/SearchResults.js
+++ musical-app/src/components/SearchResults/SearchResults.js
@@ -17,20 +17,4 @@ class SearchResults extends React.Component {
     }
 
 
-
-// class SearchResults extends React.Component {
-//     render() {
-//         return (
-//             <div className="SearchResults">
-//                 <h2>Results</h2>
-//                 {this.props.SearchResults ? (
-//                     <TrackList tracks={this.props.searchResults} onAdd={this.props.onAdd} />
-//                 ) : (
-//                     <p>Loading...</p>
-//                 )}
-//             </div>
-//         );
-//     }
-// }
-
 export default SearchResults;
